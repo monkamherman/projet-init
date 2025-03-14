1bd48213e3898d64ab9e034055f3a362c3e96311
monkamherman <cesaristos5@gmail.com>
2025-03-14 02:44:09 +0100

    init

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,38 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+// import { envs } from './src/core/config/env';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+      process: path.resolve(__dirname, 'node_modules/process'),
     },
   },
-})
+
+  server: {
+    host: process.env.HOST || '0.0.0.0',
+    port: Number(process.env.PORT) || 4000,
+  },
+
+  preview: {
+    host: process.env.HOST || '0.0.0.0',
+    port: Number(process.env.PORT) || 4000,
+  },
+
+  css: {
+    postcss: './postcss.config.js',
+  },
+});
